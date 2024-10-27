bd5eef8b3886f02e420867200b01aefbfa72a5c4
NaryDiniz <senainaryara@gmail.com>
2024-10-27 20:36:40 -0300

    testes usuarios

--- public/script.js
+++ public/script.js
@@ -2,14 +2,13 @@
 function abrirModal(){
     const modal =   document.getElementById('modal-login')
     modal.classList.add('abrir')
-  
+
     modal.addEventListener('click', (e) => {
       if(e.target.id == 'modal-login'){
           modal.classList.remove('abrir')
       }
     })
   }
-  
   function cancelarModal(){
     const modal =   document.getElementById('btn-modal')
     modal.classList.add('abrir')
@@ -20,8 +19,18 @@ function abrirModal(){
       }
     })
   }
+  function cancelarModal(){
+    const modal =   document.getElementById('fechar')
+    modal.classList.add('abrir')
   
-  function entarModal(){
+    modal.addEventListener('click', (e) => {
+      if(e.target.id == 'fechar'){
+          modal.classList.remove('abrir')
+      }
+    })
+  }
+  
+  function entrarModal(){
     const modal =   document.getElementById('btn-modal')
     modal.classList.add('abrir')
   
@@ -31,3 +40,32 @@ function abrirModal(){
       }
     })
   }
+  //Logica dos usuarios 
+  /*{
+    mini banco de dados
+  }*/ 
+  let db = [
+    {
+      email:"miguelphb@gmail.com", //1°
+      senha:"migueldsds"
+    },
+    {
+      email:"naryaradiniz@gmail.com", //2°
+      senha:"n2328n"
+    }
+  ]
+
+  const imagemLogada = ``;
+  const nickname = document.getElementById('nickname');
+  const avatarImage = document.getElementById('avatar-image');
+  
+  const logar = document.getElementById('logar').addEventListener('click', ()=>{
+    const email = document.getElementById('email').value
+    const senha = document.getElementById('senha').value
+    const leituraDeDados = db.map( dado =>{
+      if(email == dado.email && senha == dado.senha){
+        imagem
+      }
+    })
+  })
+
