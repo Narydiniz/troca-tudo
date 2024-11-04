8212fd29edded6bc47d4f81d7e01b0441c580e02
NaryDiniz <senainaryara@gmail.com>
2024-11-04 19:10:41 -0300

    erro

--- public/script.js
+++ public/script.js
@@ -1,72 +1,17 @@
-//Modal Login 
-function abrirModal(){
-    const modal =   document.getElementById('modal-login')
-    modal.classList.add('abrir')
+// Função para exibir o modal de login
+function mostrarModal() {
+  document.getElementById('login-modal').style.display = 'block';
+}
 
-    modal.addEventListener('click', (e) => {
-      if(e.target.id == 'modal-login'){
-          modal.classList.remove('abrir')
-      }
-    })
-  }
-  function cancelarModal(){
-    const modal =   document.getElementById('btn-modal')
-    modal.classList.add('abrir')
-  
-    modal.addEventListener('click', (e) => {
-      if(e.target.id == 'btn-modal'){
-          modal.classList.remove('abrir')
-      }
-    })
-  }
-  function cancelarModal(){
-    const modal =   document.getElementById('fechar')
-    modal.classList.add('abrir')
-  
-    modal.addEventListener('click', (e) => {
-      if(e.target.id == 'fechar'){
-          modal.classList.remove('fechar')
-      }
-    })
-  }
-  
-  function entrarModal(){
-    const modal =   document.getElementById('btn-modal')
-    modal.classList.add('abrir')
-  
-    modal.addEventListener('click', (e) => {
-      if(e.target.id == 'btn-modal'){
-          modal.classList.remove('abrir')
-      }
-    })
-  }
-  //Logica dos usuarios 
-  /*{
-    mini banco de dados
-  }*/ 
-  let db = [
-    {
-      email:"miguelphb@gmail.com", //1°
-      senha:"migueldsds"
-    },
-    {
-      email:"naryaradiniz@gmail.com", //2°
-      senha:"n123456n"
-    }
-  ]
+// Função para fechar o modal de login
+function fecharModal() {
+  document.getElementById('login-modal').style.display = 'none';
+}
 
-  const imagemLogada = ``;
-  const nickname = document.getElementById('nickname');
-  const avatarImage = document.getElementById('avatar-image');
-  
-  const logar = document.getElementById('logar').addEventListener('click', ()=>{
-    const email = document.getElementById('email').value
-    const senha = document.getElementById('senha').value
-    const leituraDeDados = db.forEach( dado =>{
-      if(email == dado.email && senha == dado.senha){
-       return window.alert('Logado com sucesso')
-      }else
-      return window.alert('Credenciais invalidas')
-    })
-  });
-
+// Fecha o modal ao clicar fora do conteúdo
+window.onclick = function(event) {
+  const modal = document.getElementById('login-modal');
+  if (event.target === modal) {
+      fecharModal();
+  }
+};
